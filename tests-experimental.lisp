2d24f17c607c5a7b9d6d4f93e29ab2e7094a1d20
Swapneil Singh <swapneil.singh@gmail.com>
2024-12-20 20:49:43 -0800

    Add more tests for experimental parallel-nondeterminism feature

--- tests-experimental.lisp
+++ tests-experimental.lisp
@@ -11,3 +11,23 @@
   (lparallel.kernel-util:with-temp-kernel ((serapeum:count-cpus))
     (is (equal (iota 2000)
                (sort (all-values (p-an-integer-between 0 (1- 2000))) #'<)))))
+
+(deftest test-nested-parallelism ()
+  (lparallel.kernel-util:with-temp-kernel ((serapeum:count-cpus))
+    (is (equal (sort (append (iota 1000) (iota 2000)) #'<)
+               (sort (all-values (p-an-integer-between 0 (1- (p-either 1000 2000)))) #'<)))
+    (is (equal (list :hi :hi :hi :hi)
+               (all-values (p-a-member-of '(1 2)) (p-a-member-of '(3 4)) :hi)))))
+
+(defun p-screamer-flatten (x)
+  (let ((ret (p-a-member-of x)))
+    (if (listp ret)
+        (p-screamer-flatten ret)
+        ret)))
+
+(deftest test-parallel-screamer-defun ()
+  (lparallel.kernel-util:with-temp-kernel ((serapeum:count-cpus))
+    (let ((ret (all-values (p-screamer-flatten '(1 2 3 4 (4 5 6) '(7 8 9))))))
+      (is (member 'quote ret))
+      (is (equal '(1 2 3 4 4 5 6 7 8 9)
+                 (sort (remove 'quote ret) #'<))))))
