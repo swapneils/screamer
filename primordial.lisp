461c47ed3ae561ded75417ed7fe94779a50d4f3f
Swapneil Singh <swapneis@amazon.com>
2025-01-13 19:48:56 -0800

    Fix typo in `mapcar-nondeterministic` test

--- primordial.lisp
+++ primordial.lisp
@@ -889,7 +889,7 @@
    (let ((a (list 1 2 3))
         (b (list 4 5 6))
         (c (list 7 8 9)))
-    (all-values (mapcar-nondeterministic #'a-member-of (list a b c))))
+    (all-values (screamer::mapcar-nondeterministic #'a-member-of (list a b c))))
    '((1 4 7) (1 4 8) (1 4 9) (1 5 7) (1 5 8) (1 5 9) (1 6 7) (1 6 8) (1 6 9)
      (2 4 7) (2 4 8) (2 4 9) (2 5 7) (2 5 8) (2 5 9) (2 6 7) (2 6 8) (2 6 9)
      (3 4 7) (3 4 8) (3 4 9) (3 5 7) (3 5 8) (3 5 9) (3 6 7) (3 6 8) (3 6 9))))
